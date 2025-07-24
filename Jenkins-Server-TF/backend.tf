1000a3accf925a494a83aef69b9334d160dc6335
DURGAPRASAD <mallahdurgaprasad@gmail.com>
2025-07-24 23:14:21 +0530

    Add files via upload

---
+++ Jenkins-Server-TF/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-bucketv29"
+    region         = "ap-south-1"
+    key            = "Chatbot-UI/Jenkins-TF/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+}
